1cb4771003748680b3198987033b85de64f90f6b
Adam Urbina <adamurbina@gmail.com>
2018-09-29 18:33:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,25 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def detroy
+  end
+
+  def create
+  end
+  
 end
