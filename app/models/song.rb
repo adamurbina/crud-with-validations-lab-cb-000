899df653197369ce15f6b0d245fbdb74b23653f5
Adam Urbina <adamurbina@gmail.com>
2018-09-29 20:22:44 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,12 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :release_year, presence: true, if: -> {released == true}
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: -> {released == true}
+
+  def same_song
+    if song = Song.find_by(title: title)
+      if song.release_year = release_year
+        errors.add(:title, "same song")
+      end
+    end
+  end
 end
