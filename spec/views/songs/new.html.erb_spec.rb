f9453bc8a81e45df94c76840c97cbce4fc1ec749
Adam Urbina <adamurbina@gmail.com>
2018-09-29 20:06:35 +0000

    automatically backed up by learn

--- spec/views/songs/new.html.erb_spec.rb
+++ spec/views/songs/new.html.erb_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'songs/new', type: :feature do
   it 'renders form' do
     visit new_song_path
 
-    form = find('_form')
+    form = find('form')
 
     expect(form.find('input#song_title')[:name]).to eq('song[title]')
     expect(form.find('input#song_release_year')[:name]).to eq('song[release_year]')
