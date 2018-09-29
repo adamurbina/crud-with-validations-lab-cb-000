949314aaae8713cbf5a2754dfe735fc20853db09
Adam Urbina <adamurbina@gmail.com>
2018-09-29 18:51:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,6 +7,16 @@ class SongsController < ApplicationController
   def new
   end
 
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
   def show
     @song = Song.find(params[:id])
   end
@@ -27,15 +37,7 @@ class SongsController < ApplicationController
   def detroy
   end
 
-  def create
-    @song = Song.new(song_params)
-    if @song.valid?
-      @song.save
-      redirect_to song_path(@song)
-    else
-      render :new
-    end
-  end
+  
 
   def song_params
     params.permit(:title, :released, :release_year, :artist_name, :genre)
